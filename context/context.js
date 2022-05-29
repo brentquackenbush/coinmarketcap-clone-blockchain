c36271f636ae0db6d4065998227848fdfdc7dc7b
Brent Quackenbush <brentprogramming@gmail.com>
2022-05-29 19:29:47 -0400

    commit

---
+++ context/context.js
@@ -0,0 +1,28 @@
+import { createContext, useState, useEffect } from 'react'
+import React from 'react'
+
+export const CoinMarketContext = createContext()
+
+export const CoinMarketProvider = ({children}) => {
+
+    const getTopTenCoins = async () => {
+        try {
+            const res = await fetch('/api/getTopTen')
+            const data = await res.json();
+            return data.data.data
+        } catch(e) {
+            console.log(e.message)
+        }
+        
+    }
+
+    return (
+        <CoinMarketContext.Provider
+        value = {{
+            getTopTenCoins
+        }}
+        >
+            {children}
+            </CoinMarketContext.Provider>
+    )
+}
